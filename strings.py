257dd4931a3fb24d9d86f233be70486663f75923
Bonface Masira <bonfacemasira@gmail.com>
2023-04-25 10:16:06 +0300

    learn strings

---
+++ strings.py
@@ -0,0 +1,36 @@
+a = 'Hello, world!'
+# print(a)
+
+# Multiline Strings
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+# print(b)
+
+c = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+# print(c)
+
+# print(a[1])
+
+# Looping Through a String
+# for x in 'banana':
+#  print(x)
+
+#String Length
+# print(len(a))
+
+#Check String
+txt = "The best things in life are free!"
+# print("free" in txt)
+# if "free" in txt:
+#     print("Yes, 'free' is present!")
+
+# print("expensive" not in txt)
+
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present!")
