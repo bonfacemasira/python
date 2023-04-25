bda6a115457a73760877661e6ec022eea56a4b03
Bonface Masira <bonfacemasira@gmail.com>
2023-04-25 18:09:26 +0300

    while loop

---
+++ whileloop.py
@@ -0,0 +1,18 @@
+i = 0
+while i < 100:
+    print(i)
+    if i == 10:
+        break
+    i += 1
+
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
