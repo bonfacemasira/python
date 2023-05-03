c0e0cc4fd82912d2cc9b631824a7ffa040bb5aa7
Bonface Masira <bonfacemasira@gmail.com>
2023-05-03 14:55:39 +0300

    copy lists in Python

---
+++ copylists.py
@@ -0,0 +1,9 @@
+# Copy a List
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+# print(mylist)
+
+# Another way to Copy a List
+anotherlist = list(thislist)
+print(anotherlist)
