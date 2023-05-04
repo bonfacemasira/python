b0d4ef8e9a43f15762d12f8ce858bd7aef165ef0
Bonface Masira <bonfacemasira@gmail.com>
2023-05-04 13:02:25 +0300

    loop tuple using while loop

---
+++ looptuple.py
@@ -0,0 +1,13 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+
+# Loop Through the Index Numbers
+for i in range(0, len(thistuple)):
+    print(thistuple[i])
+
+# Using a While Loop
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i += 1
