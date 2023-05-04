8deda79f60c44356682a917fb65f857e559a9d65
Bonface Masira <bonfacemasira@gmail.com>
2023-05-04 19:29:21 +0300

    change items in dictionaries through update and change

---
+++ changedict.py
@@ -0,0 +1,12 @@
+# Change Values
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+# thisdict["year"] = 2018
+print(thisdict)
+
+# Update Dictionary
+thisdict.update({"year": 2020})
+print(thisdict)
