7c071a2b2859d779bbc5ebbcaa416561d0fc8a51
Bonface Masira <bonfacemasira@gmail.com>
2023-05-05 20:30:52 +0300

    iterate through dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -4,6 +4,16 @@ thisdict = {
     "year": 1964
 }
 
-print(thisdict)
-print(type(thisdict))
-print(thisdict['year'])
+# print(thisdict)
+# print(type(thisdict))
+# print(thisdict['year'])
+def solution(data):
+    count = 0
+
+    for key, value in thisdict.items():
+        count =+ 1
+        # print(key, value)
+        print(f'{key}: Quantity: {count} Price is {value}')
+
+
+solution(thisdict)
